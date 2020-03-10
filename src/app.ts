9f8ad8b92eebd22d25b6ed9f2b9f0ac1c9fefcaf
BenKung <moon791017@gmail.com>
2020-03-10 21:27:10 +0800

    OK

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,11 @@
 import $ from 'jquery';
 import QRScanner from 'qr-code-scanner';
-
-QRScanner.initiate({
-    className:"picker",
-    // match: /^[a-zA-Z0-9]{16,18}$/, // optional
-    onResult: function (result) { console.info('DONE: ', result); },
-    onError: function (err) { console.error('ERR :::: ', err); }, // optional
-    onTimeout: function () { console.warn('TIMEOUT'); } // optional
+$('#open').click(()=>{
+    QRScanner.initiate({
+        className:"picker",
+        // match: /^[a-zA-Z0-9]{16,18}$/, // optional
+        onResult: function (result) { console.info('DONE: ', result); },
+        onError: function (err) { console.error('ERR :::: ', err); }, // optional
+        onTimeout: function () { console.warn('TIMEOUT'); } // optional
+    })
 })
