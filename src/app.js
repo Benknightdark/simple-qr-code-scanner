27b799e48fa391900b2040913816311ae40173d5
moon791017 <moon791017@gmail.com>
2020-03-11 17:38:58 +0800

    OK

--- src/app.js
+++ src/app.js
@@ -1,15 +1,14 @@
 import QrScanner from "./qr-scanner.min.js";
-import $ from 'jquery'
-((navigator,window,document,$)=>{
+((navigator, window, document, $) => {
     QrScanner.WORKER_PATH = './qr-scanner-worker.min.js';
-
+    let scanner;
     const video = document.getElementById('qr-video');
     const camHasCamera = document.getElementById('cam-has-camera');
     const camQrResult = document.getElementById('cam-qr-result');
     const camQrResultTimestamp = document.getElementById('cam-qr-result-timestamp');
     const fileSelector = document.getElementById('file-selector');
     const fileQrResult = document.getElementById('file-qr-result');
-    const checkUserMedia=()=>{
+    const checkUserMedia = () => {
         return navigator.mediaDevices && navigator.mediaDevices.getUserMedia;
     }
     function setResult(label, result) {
@@ -20,34 +19,45 @@ import $ from 'jquery'
         clearTimeout(label.highlightTimeout);
         label.highlightTimeout = setTimeout(() => label.style.color = 'inherit', 100);
     }
-    
-    // ####### Web Cam Scanning #######
-    
-    if (checkUserMedia()) {
-        QrScanner.hasCamera().then(hasCamera => camHasCamera.textContent = hasCamera);
-    
-        const scanner = new QrScanner(video, result => setResult(camQrResult, result));
-        scanner.start();
-    
-        document.getElementById('inversion-mode-select').addEventListener('change', event => {
-            scanner.setInversionMode(event.target.value);
-        });
-    }else{
-        $('#scan-web-cam').hide();
-    }
-    
-    
-    // ####### File Scanning #######
-    
-    fileSelector.addEventListener('change', event => {
-        const file = fileSelector.files[0];
-        if (!file) {
-            return;
+
+    const initQRCodeScanner = () => {
+        // ####### Web Cam Scanning #######
+
+        if (checkUserMedia()) {
+            QrScanner.hasCamera().then(hasCamera => camHasCamera.textContent = hasCamera);
+
+             scanner = new QrScanner(video, result => setResult(camQrResult, result));
+            scanner.start();
+
+            document.getElementById('inversion-mode-select').addEventListener('change', event => {
+                scanner.setInversionMode(event.target.value);
+            });
+        
+        } else {
+            $('#scan-web-cam').hide();
         }
-        QrScanner.scanImage(file)
-            .then(result => setResult(fileQrResult, result))
-            .catch(e => setResult(fileQrResult, e || 'No QR code found.'));
-    });
 
 
-})(navigator,window,document,$)
+        // ####### File Scanning #######
+
+        fileSelector.addEventListener('change', event => {
+            const file = fileSelector.files[0];
+            if (!file) {
+                return;
+            }
+            QrScanner.scanImage(file)
+                .then(result => setResult(fileQrResult, result))
+                .catch(e => setResult(fileQrResult, e || 'No QR code found.'));
+        });
+    }
+    $('#qr-code-scanner-modal').on('hidden.bs.modal', function (e) {
+        console.log('close')
+        scanner.destroy();
+        scanner = null;
+        
+      })
+      $('#qr-code-scanner-modal').on('show.bs.modal', function (e) {
+        console.log('show')
+        initQRCodeScanner();
+      })
+})(navigator, window, document, $)
